5744e0b2d9243e0b99d320ec3078c4f2d52628e2
elbab <mohamedelbabili@hotmail.com>
2019-01-30 05:42:01 +0100

    changement de readme

--- Test.java
+++ Test.java
@@ -1,6 +1,23 @@
-public class Test{
+import java.util.Scanner;
+
+public class Test {
+
 	public static void main(String[] args) {
-		System.out.println("bonjour");
-		System.out.println("coucou les ami(e)s");
+		// TODO Auto-generated method stub
+		
+		System.out.println("comment tu t'appelles ?");
+		
+		Scanner scan = new Scanner(System.in);
+		String nom = scan.nextLine();
+		
+		System.out.println("quel est ton prénom ?");
+		String prenom = scan.nextLine();
+		
+		System.out.println("bonjour "+ nom + " " +prenom);
+		System.out.println("quel âge as-tu ?");
+		int age = scan.nextInt();
+		
+		if(age >= 18)	System.out.println("tu es majeur");
+		else System.out.println("tu n'es pas majeur");
 	}
 }
